54c5d9191dbb49a2f2d1b0e40129bcdadadbd3e5
hzx174 <1744709355@qq.com>
2018-06-27 10:00:08 +0800

    1

--- app/src/main/java/com/yiyi/translater/activity/MainActivity.java
+++ app/src/main/java/com/yiyi/translater/activity/MainActivity.java
@@ -95,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
         NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
         if (networkInfo == null || !networkInfo.isAvailable()) {
             Toast.makeText(MainActivity.this,getString(R.string.neterror),Toast.LENGTH_LONG).show();
+            //jjjj
         } else {
             flag=true;
         }

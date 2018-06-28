4d7710c6d7840bbb31a045ea384420492fa9e643
HandleYu <1500726014@qq.com>
2018-06-28 11:09:21 +0800

    添加了两个fragment

---
+++ app/src/main/java/com/yiyi/translater/fragment/Fragment_ZiDian.java
@@ -0,0 +1,30 @@
+package com.yiyi.translater.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yiyi.translater.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment_ZiDian extends Fragment {
+
+
+    public Fragment_ZiDian() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_zidian, container, false);
+    }
+
+}
